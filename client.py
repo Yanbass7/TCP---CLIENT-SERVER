08bddfcc4cddaa418c68dd32ef790909ac36b6f4
Yan <87201157+Yanbass7@users.noreply.github.com>
2022-05-09 00:56:45 -0300

    Update client.py

--- client.py
+++ client.py
@@ -2,22 +2,22 @@ import os, pickle, socket, sys
 
 BUFFER_SIZE = 1024
 
-def list_cache_request(s, request):
-    s.send(request.encode())
-    files_in_cache = s.recv(BUFFER_SIZE)
-    print(pickle.loads(files_in_cache))
+def list_cache_requisicao(s, requeisicao):
+    s.send(requeisicao.encode())
+    files_cache = s.recv(BUFFER_SIZE)
+    print(pickle.loads(files_cache))
 
-def file_request(directory, s, request):
-    os.chdir(directory)
-    s.send(request.encode())
+def file_requisicao(diretorio, s, requisicao):
+    os.chdir(diretorio)
+    s.send(requisicao.encode())
 
-    with open(request, 'wb') as file:
+    with open(requisicao, 'wb') as file:
         have = True
         while True:
             data = s.recv(BUFFER_SIZE)
             if(data == b'Arquivo Inexistente'):
                 print('Arquivo Inexistente')
-                os.remove(request)
+                os.remove(requisicao)
                 have = False
                 break
             if not data:
@@ -25,20 +25,20 @@ def file_request(directory, s, request):
             file.write(data)
     file.close()
     if(have):
-        print(f'Arquivo {request} salvo com sucesso')
+        print(f'Arquivo {requisicao} salvo com sucesso')
     s.close()
 
 if __name__ == "__main__":
     
     HOST = sys.argv[1]
     PORT = sys.argv[2]
-    request = sys.argv[3]
+    requisicao = sys.argv[3]
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, int(PORT)))
 
-    if(request == 'list'):
-        list_cache_request(s, request)
+    if(requisicao == 'list'):
+        list_cache_requisicao(s, requisicao)
     else:
-        DIRECTORY = sys.argv[4]
-        file_request(DIRECTORY, s, request)
+        DIRETORIO = sys.argv[4]
+        file_requisicao(DIRETORIO, s, requisicao)
